8b7ae8a06d3a11c22b06985a845af8629a6a0ca0
On <ontamam@gmail.com>
2023-05-03 13:05:40 +0300

    level generation ready to go, can start creating dungeons

--- IdoFinal/Assets/Scripts/LevelGenerator.cs
+++ IdoFinal/Assets/Scripts/LevelGenerator.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEditor;
 
 
 public class LevelGenerator : MonoBehaviour
@@ -12,13 +9,13 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private MyTile fillTile;
     [SerializeField] private int width;
     [SerializeField] private int length;
+    [SerializeField] private int seed;
     [SerializeField] private float scale;
     [SerializeField] private int octaves;
-    [SerializeField] private float persistence;
+    [SerializeField, Range(0, 1)] private float persistence;
     [SerializeField] private float lacunarity;
     [SerializeField] private Vector2 offset;
     [SerializeField] private BiomeLayer biomeLayer;
-    PerlinNoiseTest noise;
 
     [ContextMenu("test spawning prefab")]
 
@@ -29,16 +26,36 @@ public class LevelGenerator : MonoBehaviour
 
     private void TestSpawn()
     {
-        noise = new PerlinNoiseTest(width, length, scale, octaves, persistence, lacunarity, offset);
-        float[,] heightMap = noise.Generate();
+        float[,] heightMap = Noise.GenerateNoiseMap(width, length, seed, scale, octaves, persistence, lacunarity, offset);
         for (int i = 0; i < heightMap.GetLength(0); i++)
         {
             for (int j = 0; j < heightMap.GetLength(1); j++)
             {
-                testTileMap.SetTile(new Vector3Int(i, j, 0), biomeLayer.GetTileFromHeight(heightMap[i,j]));
+                testTileMap.SetTile(new Vector3Int(i, j, 0), biomeLayer.GetTileFromHeight(heightMap[i, j]));
             }
         }
         StartCoroutine(GameManager.Instance.LevemManager.CurrentLevel.StartUpLevel());
+    }
+
+    private void OnValidate()
+    {
+        if (width < 1)
+        {
+            width = 1;
+        }
+        if (length < 1)
+        {
+            length = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+
     }
 
 }
